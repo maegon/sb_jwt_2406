82902d7c43ec0b625e435c5a391c60f60ae72c6f
maegon <dlagorhs99@naver.com>
2024-06-10 15:49:51 +0900

    add gradle, hmac encoded

--- src/test/java/com/example/jwt/JwtApplicationTests.java
+++ src/test/java/com/example/jwt/JwtApplicationTests.java
@@ -1,9 +1,14 @@
 package com.example.jwt;
 
+import io.jsonwebtoken.security.Keys;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import javax.crypto.SecretKey;
+import java.nio.charset.StandardCharsets;
+import java.util.Base64;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -11,11 +16,20 @@ import static org.assertj.core.api.Assertions.assertThat;
 class JwtApplicationTests {
 
 	@Value("${custom.jwt.secretKey}")
-	private String secretKey;
+	private String secretKeyPlain;
 
 	@Test
 	@DisplayName("시크릿 키 존재 여부 체크")
 	void test1() {
+		assertThat(secretKeyPlain).isNotNull();
+	}
+
+	@Test
+	@DisplayName("시크릿 키 플레인을 이용하여 암호화 알고리즘 시크릿 키 객체 생성")
+	void test2() {
+		String keyBase64Encoded = Base64.getEncoder().encodeToString(secretKeyPlain.getBytes());
+
+		SecretKey secretKey = Keys.hmacShaKeyFor(keyBase64Encoded.getBytes());
 		assertThat(secretKey).isNotNull();
 	}
 
