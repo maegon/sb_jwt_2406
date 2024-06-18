14ae31f951f45a8463edf5c759c39cbb71c50e0d
maegon <dlagorhs99@naver.com>
2024-06-18 16:30:16 +0900

    article patch

--- src/test/java/com/example/jwt/domain/article/ArticleControllerTest.java
+++ src/test/java/com/example/jwt/domain/article/ArticleControllerTest.java
@@ -16,8 +16,7 @@ import org.springframework.test.web.servlet.ResultActions;
 import java.awt.*;
 import java.nio.charset.StandardCharsets;
 
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -94,5 +93,35 @@ public class ArticleControllerTest {
                 .andExpect(jsonPath("$.resultCode").value("S-3"))
                 .andExpect(jsonPath("$.msg").exists())
                 .andExpect(jsonPath("$.data.article.id").exists());
+    }
+
+
+    @Test
+    @DisplayName("POST /articles/2")
+    @WithUserDetails("admin")
+    void t4() throws Exception {
+        // When
+        ResultActions resultActions = mvc
+                .perform(
+                        patch("/api/v1/articles/2")
+                                .content("""
+                                        {
+                                            "subject": "제목 2222 !!!",
+                                            "content": "내용 2222 !!!"
+                                        }
+                                        """)
+                                .contentType(new MediaType(MediaType.APPLICATION_JSON, StandardCharsets.UTF_8))
+                )
+                .andDo(print());
+
+        // Then
+        resultActions
+                .andExpect(status().is2xxSuccessful())
+                .andExpect(jsonPath("$.resultCode").value("S-4"))
+                .andExpect(jsonPath("$.msg").exists())
+                .andExpect(jsonPath("$.data.article.id").value(2))
+                .andExpect(jsonPath("$.data.article.subject").value("제목 2222 !!!"))
+                .andExpect(jsonPath("$.data.article.content").value("내용 2222 !!!"));
+
     }
 }
